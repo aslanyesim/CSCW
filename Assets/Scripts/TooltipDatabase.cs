1e4c5ce8dbde816d31ba7a229da5fc08e6348fd5
Deniz Bicer <dnzbicer@gmail.com>
2015-11-29 20:15:18 +0100

    Project references for LitJson plugin is corrected.

---
+++ Assets/Scripts/TooltipDatabase.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using LitJson;
+using System.Collections.Generic;
+
+public class TooltipDatabase : MonoBehaviour {
+	private List<Plate> plateDatabase = new List<Plate>();
+
+}
+public class Plate
+{
+
+}
