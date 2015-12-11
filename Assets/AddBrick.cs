0cdcdc829e799cd98fa5a8f7629d480dc489027a
aslanyesim <aslanyesim@outlook.com>
2015-12-11 12:49:46 +0100

    First prototype of A to plate with script.

--- Assets/AddBrick.cs
+++ Assets/AddBrick.cs
@@ -1,44 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-using System.Collections.Generic;
+public class AddBrick : MonoBehaviour
+{
+    /*void Start()
+    {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        Renderer rend = go.GetComponent<Renderer>();
+        rend.material.mainTexture = Resources.Load("glass") as Texture;
+    }*/
 
-public class AddBrick : MonoBehaviour, ITrackableEventHandler {
-  private TrackableBehaviour mTrackableBehaviour;
-  public Transform myModelPrefab;
-  // Use this for initialization
-  void Start ()
-  {
-    mTrackableBehaviour = GetComponent<TrackableBehaviour>();
-    if (mTrackableBehaviour) {
-      mTrackableBehaviour.RegisterTrackableEventHandler(this);
-    }
-  }
-  // Update is called once per frame
-  void Update ()
-  {
-  }
-  public void OnTrackableStateChanged(
-    TrackableBehaviour.Status previousStatus,
-    TrackableBehaviour.Status newStatus)
-  { 
-    if (newStatus == TrackableBehaviour.Status.DETECTED ||
-        newStatus == TrackableBehaviour.Status.TRACKED ||
-        newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+    public GameObject prefab;
+    public float gridX = 5f;
+    public float gridY = 5f;
+    public float spacing = 2f;
+    public float high = 0.1f;
+
+    int[,] BrickTable = new int[5, 5] {{1,0,0,0,1},
+                                      {1,1,1,1,1}, 
+                                      {1,0,0,0,1},
+                                      {0,1,0,1,0},
+                                      {0,0,1,0,0}, };
+
+    void Start()
     {
-      OnTrackingFound();
+        for (int y = 0; y < gridY; y++)
+        {
+            for (int x = 0; x < gridX; x++)
+            {
+                if (BrickTable[y, x] == 1) {
+                    Vector3 pos = new Vector3(x, high, y) * spacing;
+                    Instantiate(prefab, pos, Quaternion.identity);
+                    }
+            }
+        }
     }
-  } 
-  private void OnTrackingFound()
-  {
-    if (myModelPrefab != null)
-    {
-      Transform myModelTrf = GameObject.Instantiate(myModelPrefab) as Transform;
-      myModelTrf.parent = mTrackableBehaviour.transform;
-      myModelTrf.localPosition = new Vector3(0f, 0f, 0f);
-      myModelTrf.localRotation = Quaternion.identity;
-      myModelTrf.localScale = new Vector3(0.0005f, 0.0005f, 0.0005f);
-      myModelTrf.gameObject.active = true;
-    }
-  }
+
 }
